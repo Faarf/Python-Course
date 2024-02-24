dbb6ffaa30b8574bc95d8ecf056e46e53677abb9
Markus Gustafsson <markus_faarf@hotmail.com>
2024-02-24 14:18:30 +0100

    Create Einstein.py
    Short code to calculate the amount of Jules based on the mass taken from Einstein's famous E = mc2 equation.

---
+++ Einstein.py
@@ -0,0 +1,11 @@
+def einstein():
+    # Input in mass, times c two times (squared)
+    energy = int(input())*(300000000*300000000)
+    return energy
+
+# Assort the previous value to the variable "energy" and print output
+def main():
+    energy = einstein()
+    print(energy)
+
+main ()
